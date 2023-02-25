b02eff549516ea26e7490f276fa27271a040bfdd
Imran-2022 <mdimranulhaque.202@gmail.com>
2023-02-25 20:37:20 +0600

    convert html to react

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from "../assets/logo.svg"
+import Search from './Search';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav class="py-4 2xl:px-6">
+                <div class="container flex items-center justify-between">
+                    <img src={logo} width="150px" class="object-contain" />
+
+                    <ul class="hidden md:flex items-center space-x-6">
+                        <li class="font-semibold cursor-pointer">Book Store</li>
+                        <li class="cursor-pointer">Wishlist</li>
+                        <li class="cursor-pointer">My Collection</li>
+                    </ul>
+
+                    <Search/>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
